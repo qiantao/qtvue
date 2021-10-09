942c321ba8195e33d205f3182bd7c2ce06cbb483
muyang <709773414@qq.com>
2021-10-09 16:30:56 +0800

    1
    1

--- src/pages/head/head.js
+++ src/pages/head/head.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import Head from './Head.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-var app = new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+var home = new Vue({
+    el: '#head',//html中的id为home的div
+    router,
+    components: { Head },
+    template: '<Head/>'
 })
+
+
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import Home from './Home.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-var app = new Vue({
-  el: '#app',
+var home = new Vue({
+  el: '#home',//html中的id为home的div
   router,
-  components: { App },
-  template: '<App/>'
+  components: { Home },
+  template: '<Home/>'
 })
+
+var home = new Vue({
+  el: '#home',//html中的id为home的div
+  router,
+  components: { Home },
+  template: '<Home/>'
+})
+
